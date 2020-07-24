1a7802b069a3191da617389d29a325d790c30c74
iamvar <iamvar@gmail.com>
2020-07-24 22:49:26 +0300

    fake binary

---
+++ php/8 kyu/fake_binary.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+//https://www.codewars.com/kata/fake-binary
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fake_bin(string $s): string
+{
+    return preg_replace(['/[1-4]/', '/[5-9]/'], ['0', '1'], $s);
+}
