ee73ea0a1d14f664f206c27932237212706ce823
iamvar <iamvar@gmail.com>
2020-08-01 14:59:54 +0300

    removed unused print

--- python/4 kyu/sudoku_solution_validator.py
+++ python/4 kyu/sudoku_solution_validator.py
@@ -35,8 +35,6 @@
 def valid_solution(board):
     from collections import defaultdict
 
-    blocks = [[board[x+a][y+b] for a in (0, 1, 2) for b in (0, 1, 2)] for x in (0, 3, 6) for y in (0, 3, 6)]
-    print(blocks)
     sets = defaultdict(list)
     for i, row in enumerate(board):
         for j, num in enumerate(row):
