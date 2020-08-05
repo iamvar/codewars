79198afb2af71cbb42b967424b8493e8f5ebe223
iamvar <iamvar@gmail.com>
2020-08-05 20:30:11 +0300

    fixed indents

--- php/6 kyu/crack_the_pin.php
+++ php/6 kyu/crack_the_pin.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 //https://www.codewars.com/kata/crack-the-pin
 
 function crack($hash) {
-  for($x = 0; $x <= 99999; $x++) {
-	  if (md5($s = sprintf('%05d', $x)) === $hash) {
-		 return $s; 
-	  }
-  }
+    for($x = 0; $x <= 99999; $x++) {
+        if (md5($s = sprintf('%05d', $x)) === $hash) {
+            return $s; 
+        }
+    }
 }
