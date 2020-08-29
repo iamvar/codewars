7407d720af1f6949174e03c60dc5a34e93206ed0
iamvar <iamvar@gmail.com>
2020-08-29 14:59:03 +0300

    add mumbling kata - php

---
+++ php/7 kyu/mumbling.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+//https://www.codewars.com/kata/mumbling
+//This time no story, no theory. The examples below show you how to write function accum:
+//
+//Examples:
+//
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(string $s): string {
+    $res = [];
+    foreach (str_split(strtolower($s)) as $i => $c) {
+        $res[] = ucfirst(str_repeat($c, $i + 1));
+    }
+
+    return implode('-', $res);
+}
