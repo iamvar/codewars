02c510bb142ff052e1c63c21a53665b9d580ba57
iamvar <iamvar@gmail.com>
2020-10-26 15:19:24 +0200

    added find-the-odd-int - php

---
+++ php/6 kyu/find_the_odd_int.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @link https://www.codewars.com/kata/find-the-odd-int
+ *
+ * Given an array of integers, find the one that appears an odd number of times.
+ * There will always be only one integer that appears an odd number of times.
+ */
+function findIt(array $seq) : int
+{
+    return key(array_filter(array_count_values($seq), function($v) {
+        return $v & 1;
+    }));
+}
