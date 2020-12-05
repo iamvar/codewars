ba6b2600b47f56b31d24c5fb23b1369785d98e5a
iamvar <iamvar@gmail.com>
2020-12-05 10:08:14 +0200

    added kata reversed-sequence

---
+++ php/8 kyu/reversed_sequence.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @link https://www.codewars.com/kata/reversed-sequence
+ *
+ * Build a function that returns an array of integers from n to 1 where n>0.
+ * Example : n=5 >> [5,4,3,2,1]
+ */
+function reverseSeq ($n) {
+    return range($n, 1);
+}
