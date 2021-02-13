8fa58ef7a3468be0e4e276f1429360ba027b7b5a
iamvar <iamvar@gmail.com>
2021-02-13 12:58:17 +0200

    added "who likes it" kata (php)

---
+++ php/6 kyu/who_likes_it.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @link https://www.codewars.com/kata/who-likes-it
+ * You probably know the "like" system from Facebook and other pages.
+ * People can "like" blog posts, pictures or other items.
+ * We want to create the text that should be displayed next to such an item.
+ * Implement a function likes :: [String] -> String, which must take in input array,
+ * containing the names of people who like an item. It must return the display text as shown in the examples:
+ * likes [] -- must be "no one likes this"
+ * likes ["Peter"] -- must be "Peter likes this"
+ * likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+ * likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+ * likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+ * For 4 or more names, the number in and 2 others simply increases.
+ */
+function likes(array $names): string
+{
+    switch ($cnt = count($names)) {
+        case 0:
+            return 'no one likes this';
+        case 1:
+            return "$names[0] likes this";
+        case 2:
+            return "$names[0] and $names[1] like this";
+        case 3:
+            return "$names[0], $names[1] and $names[2] like this";
+        default:
+            return sprintf("$names[0], $names[1] and %d others like this", $cnt - 2);
+    }
+}
